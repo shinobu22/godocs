b031e1a41fd51286161f4feea28c823eb13c0c87
shinobu22 <at_sekson@atcreative.internal>
2025-07-22 15:52:00 +0700

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'shinobu22', // Usually your GitHub org/user name.
   projectName: 'godocs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
